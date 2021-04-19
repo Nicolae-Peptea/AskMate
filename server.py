ba7e86cf6886348a9b5d48190804c731fa56b020
Nicolae-Peptea <nicolae.peptea@gmail.com>
2021-04-19 15:38:03 +0300

    read file

--- server.py
+++ server.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request
+import data_handler
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/list")
+def list_questions():
+    return render_template('list.html')
 
+
+@app.route("/question/<int:question_id>")
+def display_question(quenstion_id):
+    pass
 
 if __name__ == "__main__":
     app.run()
